8390c017a488b747a4194022b74c6d82f78ff6bd
Louise <louise.k.s@live.se>
2023-05-23 14:59:55 +0200

    added "add company" form + linked to database

---
+++ main.js
@@ -0,0 +1,70 @@
+document.getElementById('companyForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); 
+  
+    // hämtar data från formulär
+    var companyName = document.querySelector('input[name="companyName"]').value;
+    var companyLogo = document.querySelector('input[name="companyLogo"]').value;
+    var companyLocation = document.querySelector('input[name="companyLocation"]').value;
+    var companyAbout = document.querySelector('textarea[name="companyAbout"]').value;
+    var companyLookingFor = Array.from(document.querySelectorAll('input[name="companyLookingFor"]:checked')).map(function(checkbox) {
+      return checkbox.value;
+    });
+    var companyImages = Array.from(document.querySelectorAll('input[name="companyImages"]')).map(function(input) {
+      return input.value;
+    });
+    var companyWebpage = document.querySelector('input[name="companyWebpage"]').value;
+    var companyChoices = {
+      choice1: document.querySelector('input[name="companyChoices.choice1"]:checked').value,
+      choice2: document.querySelector('input[name="companyChoices.choice2"]:checked').value,
+      choice3: document.querySelector('input[name="companyChoices.choice3"]:checked').value,
+      choice4: document.querySelector('input[name="companyChoices.choice4"]:checked').value,
+      choice5: document.querySelector('input[name="companyChoices.choice5"]:checked').value,
+      choice6: document.querySelector('input[name="companyChoices.choice6"]:checked').value,
+      choice7: document.querySelector('input[name="companyChoices.choice7"]:checked').value,
+      choice8: document.querySelector('input[name="companyChoices.choice8"]:checked').value,
+      choice9: document.querySelector('input[name="companyChoices.choice9"]:checked').value,
+    };
+  
+    // objekt av all info som skrivits in i formet
+    var companyData = {
+      companyName: companyName,
+      companyLogo: companyLogo,
+      companyLocation: companyLocation,
+      companyAbout: companyAbout,
+      companyLookingFor: companyLookingFor,
+      companyImages: companyImages,
+      companyWebpage: companyWebpage,
+      companyChoices: companyChoices
+    };
+  
+    const companiesInDatabase = await getCompanyData();
+    const amountOfCompanies = companiesInDatabase.length;
+    postCompanyData(companyData,amountOfCompanies)
+    .then(
+        document.getElementById('companyForm').reset()
+    )
+
+    //console.log(companyData)
+  
+  });
+
+  async function getCompanyData(){
+    const url = `https://digitalia-e9f5c-default-rtdb.europe-west1.firebasedatabase.app/admin/companies.json`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+   async function postCompanyData(obj,index){
+    const url = `https://digitalia-e9f5c-default-rtdb.europe-west1.firebasedatabase.app/admin/companies/${index}.json`;
+  
+    const init = {
+      method: "PUT",
+      body: JSON.stringify(obj),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    };
+    await fetch(url, init);
+  }
