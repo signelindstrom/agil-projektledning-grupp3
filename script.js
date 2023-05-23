a56d628a5b0acd139a7cd64389109dab80e5c4f0
majahristova <maja.pau99@gmail.com>
2023-05-23 13:56:45 +0100

    fixing basic html

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const toggleBtn = document.querySelector('.navbar_togglebutton');
-const menu = document.querySelector('.navbar_menu');
-const icons = document.querySelector('.navbar_icons');
+// const toggleBtn = document.querySelector('.navbar_togglebutton');
+// const menu = document.querySelector('.navbar_menu');
+// const icons = document.querySelector('.navbar_icons');
 
-toggleBtn.addEventListener('click', (e) => {
-e.preventDefault();
-menu.classList.add("active");
-icons.classList.add("active");
-})
+// toggleBtn.addEventListener('click', (e) => {
+// e.preventDefault();
+// menu.classList.add("active");
+// icons.classList.add("active");
+// })
 
+
+///???????
