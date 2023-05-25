ef11b49bceddc8859976bdb11dff4477ad4cfd42
radikalkatten <112856496+radikalkatten@users.noreply.github.com>
2023-05-26 01:24:37 +0200

    bit changed in the logic and added links to matching site

--- match.js
+++ match.js
@@ -1,4 +1,4 @@
-const studentInfo = {city: ["Malmö", "Göteborg"],
+const studentInfo = {city: ["Göteborg"],
 major: "frontend",
 answers: [1, 2 , 2, 1, 2, 1, 1, 2, 1, 1]}
 
@@ -12,27 +12,38 @@ async function getCompany(){
     console.log(data)
     return data; 
 }
+console.log(localStorage)
+async function getStudent(){
+  const studentID = localStorage.getItem("studentId")
+  // console.log(studentID)
+  const url = `https://digitalia-e9f5c-default-rtdb.europe-west1.firebasedatabase.app/students/${studentID}.json`
+
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data)
+  return data; 
+}
 
 async function compare(){
   const companies = await getCompany()
-  // console.log(companies[1].companyLookingFor)
+  const student = await getStudent()
+  console.log(student)
   for(let i = 0; i < companies.length; i++){
-    let isReq = compareReq(studentInfo.major, companies[i].companyLookingFor)
-    // console.log(isReq)
+    let isReq = compareReq(student.matchingValues.lookingFor, companies[i].companyLookingFor)
+    console.log(isReq)
     if(isReq){
-      isReq = compareCity(studentInfo.city, companies[i].companyCity)
-    }else{
-      return
+      isReq = compareCity(student.matchingValues.cities, companies[i].companyCity)
+      console.log(student.matchingValues.cities)
     }
-    // console.log(isReq)
+    console.log(isReq)
     if(isReq){
-      compareAnswers(studentInfo.answers, companies[i].companyChoices)
+      compareAnswers(student.matchingValues.Answers, companies[i].companyChoices)
     }
   }
 }
 
 function compareAnswers(studentAnswers, companyAnswers){
-  console.log("new check")
+  console.log(studentAnswers)
   let compatability = 0
   for(let i = 0; i < studentAnswers.length; i++){
     if(studentAnswers[i] == companyAnswers[`choice${i}`]){
@@ -42,26 +53,33 @@ function compareAnswers(studentAnswers, companyAnswers){
   }
   if(compatability >= 7){
     console.log("match")
+    return true
+  }else{
+    console.log("no match")
   }
+  return false
 }
 function compareCity(studentCities, companyCity){
+  // console.log(studentCities.length)
   for(let i = 0; i < studentCities.length; i++){
     if(studentCities[i] === companyCity){
+      console.log(studentCities)
       console.log(companyCity)
       return true
     }else{
-      return false
+      console.log("try again lmao")
     }
   }
+  return false
 }
 function compareReq(studentreq, companyreqarr){
+  // console.log(studentreq)
   for(let i = 0; i < companyreqarr.length; i++){
     let companyreq = companyreqarr[i]
     if(studentreq === companyreq){
       return true
-    }else{
-      return false
     }
   }
+  return false
 }
 compare()
