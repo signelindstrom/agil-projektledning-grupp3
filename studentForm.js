ad1973e0538c680c664524a7dda4ae37b37139fe
radikalkatten <112856496+radikalkatten@users.noreply.github.com>
2023-05-30 15:20:22 +0200

    bbb

---
+++ studentForm.js
@@ -0,0 +1,29 @@
+function toggleForm(formId) {
+  const form = document.getElementById(formId);
+  form.classList.toggle('inactive');
+}
+function hideAllForms(formName) {
+  const forms = document.getElementsByClassName('kommunForm');
+  for (let i = 0; i < forms.length; i++) {
+    if(forms[i].id !== formName){
+      forms[i].classList.add('inactive');
+    }
+  }
+}
+document.getElementById('btn1').addEventListener('click', function() {
+  hideAllForms('form1')
+  toggleForm('form1');
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+  hideAllForms('form2')
+  toggleForm('form2');
+});
+
+document.getElementById('btn3').addEventListener('click', function() {
+  hideAllForms('form3')
+  toggleForm('form3');
+});
+
+const studentForm = document.getElementById('studentForm')
+console.log(studentForm.elements)
